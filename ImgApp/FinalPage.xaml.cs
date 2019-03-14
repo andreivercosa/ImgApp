4d73fa99deb460b6b4973429132b775d3b52b70a
andreivercosa <andreivercosa@gmail.com>
2019-03-13 20:31:24 -0400

    v1

---
+++ ImgApp/FinalPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace ImgApp
+{
+    public partial class FinalPage : ContentPage
+    {
+        public FinalPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ ImgApp/PageTwo.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace ImgApp
+{
+    public partial class PageTwo : ContentPage
+    {
+        public PageTwo()
+        {
+            InitializeComponent();
+        }
+    }
+}
